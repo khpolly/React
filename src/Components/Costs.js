4ae7080adfa73b25462ba2c12e5ad43faa9a57c8
khpolly <rakpolina@gmail.com>
2022-07-26 19:56:44 +0300

    first commit

--- src/Components/Costs.js
+++ src/Components/Costs.js
@@ -1,29 +1,27 @@
 import CostItem from "./CostItem";
-import './Costs.css';
+import "./Costs.css";
+import Card from "./Card";
 
-
-function Costs (props){
-
-
-    return(
-        <div className="costs">
-      <CostItem 
-      date={props.costs[0].date} 
-      discription={props.costs[0].discription}
-      amount={props.costs[0].amount} 
+function Costs(props) {
+  return (
+    <Card className="costs">
+      <CostItem
+        date={props.costs[0].date}
+        discription={props.costs[0].discription}
+        amount={props.costs[0].amount}
       />
-      <CostItem 
-      date={props.costs[1].date} 
-      discription={props.costs[1].discription}
-      amount={props.costs[1].amount} 
+      <CostItem
+        date={props.costs[1].date}
+        discription={props.costs[1].discription}
+        amount={props.costs[1].amount}
       />
-      <CostItem 
-      date={props.costs[2].date} 
-      discription={props.costs[2].discription}
-      amount={props.costs[2].amount} 
+      <CostItem
+        date={props.costs[2].date}
+        discription={props.costs[2].discription}
+        amount={props.costs[2].amount}
       />
-        </div>
-    );
+    </Card>
+  );
 }
 
 export default Costs;
